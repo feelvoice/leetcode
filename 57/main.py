46100291b6084cbc112918f90746fed1c2ce0183
bradelement <bradelementzju@gmail.com>
2015-09-09 17:44:57 +0800

    water water..

---
+++ 57/main.py
@@ -0,0 +1,37 @@
+# Definition for an interval.
+# class Interval(object):
+#     def __init__(self, s=0, e=0):
+#         self.start = s
+#         self.end = e
+
+class Solution(object):
+    def merge(self, intervals):
+        """
+        :type intervals: List[Interval]
+        :rtype: List[Interval]
+        """
+        if not intervals: return []
+        intervals = sorted(intervals, key=lambda x:(x.start, x.end))
+
+        ret = []
+        cur = intervals[0]
+        for i in intervals[1:]:
+            if i.start <= cur.end:
+                cur.end = max(cur.end, i.end)
+            else:
+                ret.append(cur)
+                cur = i
+
+        ret.append(cur)
+        return ret
+        
+    def insert(self, intervals, newInterval):
+        """
+        :type intervals: List[Interval]
+        :type newInterval: Interval
+        :rtype: List[Interval]
+        """
+        intervals.append(newInterval)
+        return self.merge(intervals)
+            
+            
