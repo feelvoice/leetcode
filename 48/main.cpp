5b11374ebd9964c4e2c07727a740a6e715c02a95
bradelement <bradelementzju@gmail.com>
2015-09-09 11:00:19 +0800

    ...

--- 48/main.cpp
+++ 48/main.cpp
@@ -23,3 +23,10 @@ public:
         }
     }
 };
+/* ......... woca
+int temp = matrix[i][j];
+matrix[i][j] = matrix[n-1-j][i];
+matrix[n-1-j][i] = matrix[n-1-i][n-1-j];
+matrix[n-1-i][n-1-j] = matrix[j][n-1-i];
+matrix[j][n-1-i] = temp;
+*/
