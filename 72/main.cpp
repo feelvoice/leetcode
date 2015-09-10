4358b7aa4d347fb770840647de3d67eb4d9fcfc3
bradelement <bradelementzju@gmail.com>
2015-09-10 15:35:28 +0800

    dp..

---
+++ 72/main.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int len1 = word1.length();
+        int len2 = word2.length();
+        vector<vector<int> > dp(len1+1, vector<int>(len2+1));
+        for (int i=1; i<=len1; i++) {
+            dp[i][0] = i;
+        }
+        for (int i=1; i<=len2; i++) {
+            dp[0][i] = i;
+        }
+        
+        for (int i=1; i<=len1; i++) {
+            for (int j=1; j<=len2; j++) {
+                if (word1[i-1] == word2[j-1]) {
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else {
+                    dp[i][j] = min(dp[i-1][j-1], min(dp[i-1][j], dp[i][j-1])) + 1;
+                }
+            }
+        }
+        return dp[len1][len2];
+    }
+};
