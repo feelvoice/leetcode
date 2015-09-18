a25f0e082f34cce8f2686b4eec14aa2dbb8b4760
bradelement <bradelementzju@gmail.com>
2015-09-18 11:02:28 +0800

    water water..

---
+++ 102/main.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+import Queue
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        ret = []
+        if not root: return ret
+        
+        que = Queue.Queue()
+        que.put(root)
+        que.put(None)
+        
+        level = []
+        while not que.empty():
+            cur = que.get()
+            if cur is None:
+                ret.append(level)
+                level = []
+                if not que.empty():
+                    que.put(None)
+            else:
+                level.append(cur.val)
+                if cur.left:  que.put(cur.left)
+                if cur.right: que.put(cur.right)
+                
+        return ret
