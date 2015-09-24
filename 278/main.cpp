ad8a3571b340082d293b26d12ec0b4536b69fa8d
bradelement <bradelementzju@gmail.com>
2015-09-24 14:52:23 +0800

    binary search..

---
+++ 278/main.cpp
@@ -0,0 +1,19 @@
+// Forward declaration of isBadVersion API.
+bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int left = 1, right = n, mid;
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+            if (isBadVersion(mid)) {
+                right = mid - 1;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+};
