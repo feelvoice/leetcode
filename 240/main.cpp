5dad64e7d875deaa1d1ce838f184aa95d53a4c43
bradelement <bradelementzju@gmail.com>
2015-09-25 16:44:06 +0800

    review this~

---
+++ 240/main.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int row = matrix.size();
+        if (row == 0) return false;
+        int col = matrix[0].size();
+        if (col == 0) return false;
+        
+        int i=0, j=col-1;
+        while (i<row && j>=0) {
+            if (matrix[i][j] == target) return true;
+            else if (matrix[i][j] > target) j--;
+            else i++;
+        }
+        return false;
+    }
+};
